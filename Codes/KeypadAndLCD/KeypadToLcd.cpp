f755e276ae0ebdbff407891e9fe4b912da87db9c
diya <43806216+diya@users.noreply.github.com>
2019-11-21 01:17:13 +0800

    ...

--- Codes/KeypadAndLCD/KeypadToLcd.cpp
+++ Codes/KeypadAndLCD/KeypadToLcd.cpp
@@ -1,3 +1,16 @@
+//实验组件：4x4 Keypad一个，16x2 I2C LCD显示模块一个
+
+//组件描述（4x4 Keypad）：
+//8个接口，横竖电路各4个，左4为行，右4为列，全部接信号
+//组件描述（16x2 I2C LCD显示模块）：
+//4个接口，按模块接口编号默认接线
+
+//接线要求：
+//Keypad正面朝上，从左至右依次接18，19，23，5，13，12，14，27
+//I2C LCD显示模块按编号默认接线
+
+//正常输出：LCD显示模块按Keypad键入进行输出
+
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
@@ -18,6 +31,8 @@ byte colPins[COLS] = {13, 12, 14, 27};
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+//默认0x27，0x27是根据I2CDetect所得I2C模块位置编号
+//16，2表示LCD显示模式为16x2
 
 void setup(){
   Serial.begin(9600);
--- workBench/workBench.ino
+++ workBench/workBench.ino
@@ -1,43 +1,17 @@
-int red_light_pin= 17;
-int green_light_pin = 25;
-int blue_light_pin = 26;
+
+int pinRelay = 14; //管脚D3连接到继电器模块的信号脚
+ 
 void setup() {
-  //setup channel 0 with frequency 312500 Hz
-  sigmaDeltaSetup(0, 312500);
-  //attach pin 18 to channel 0
-  sigmaDeltaAttachPin(red_light_pin,0);
-  
-  //setup channel 1 with frequency 312500 Hz
-  sigmaDeltaSetup(1, 312500);
-  //attach pin 18 to channel 1
-  sigmaDeltaAttachPin(green_light_pin,1);
-  
-  //setup channel 2 with frequency 312500 Hz
-  sigmaDeltaSetup(2, 312500);
-  //attach pin 18 to channel 2
-  sigmaDeltaAttachPin(blue_light_pin,2);
+  Serial.begin(9600);
+  pinMode(pinRelay, OUTPUT); //设置pinRelay脚为输出状态
 }
-void loop() {
-  RGB_color(255, 0, 0); // Red
-  delay(1000);
-  RGB_color(0, 255, 0); // Green
-  delay(1000);
-  RGB_color(0, 0, 255); // Blue
-  delay(1000);
-  RGB_color(255, 255, 125); // Raspberry
-  delay(1000);
-  RGB_color(0, 255, 255); // Cyan
-  delay(1000);
-  RGB_color(255, 0, 255); // Magenta
-  delay(1000);
-  RGB_color(255, 255, 0); // Yellow
-  delay(1000);
-  RGB_color(255, 255, 255); // White
-  delay(1000);
-}
-void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
- {
-  sigmaDeltaWrite(red_light_pin, red_light_value);
-  sigmaDeltaWrite(green_light_pin, green_light_value);
-  sigmaDeltaWrite(blue_light_pin, blue_light_value);
+ 
+void loop() { 
+   digitalWrite(pinRelay, HIGH);//输出HIGH电平,继电器模块闭合
+   Serial.println("Closed");
+   delay(5000); //等待5000毫秒
+ 
+   digitalWrite(pinRelay, LOW);//输出LOW电平,继电器模块断开
+   Serial.println("Opened");
+   delay(8000); //等待8000毫秒
 }
