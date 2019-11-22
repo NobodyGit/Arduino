373b968ddfd45f89bfa5b50d7e2f1a6d462034a4
diya <43806216+diya@users.noreply.github.com>
2019-11-23 04:10:21 +0800

    ...

--- workBench/workBench.ino
+++ workBench/workBench.ino
@@ -1,15 +1,12 @@
-int pinRelay = 16; //管脚D3连接到继电器模块的信号脚
- 
-void setup() {
-  pinMode(pinRelay, OUTPUT); //设置pinRelay脚为输出状态
-}
- 
-void loop() { 
-  pinMode(pinRelay, OUTPUT); //设置pinRelay脚为输出状态
-//   digitalWrite(pinRelay, HIGH);//输出HIGH电平,继电器模块闭合
-   delay(3000); //等待5000毫秒
+int speakerPIN = 4;
 
- pinMode(pinRelay, INPUT); //设置pinRelay脚为输出状态
-//   digitalWrite(pinRelay, LOW);//输出LOW电平,继电器模块断开
-   delay(3000); //等待8000毫秒
+void setup(){
+  pinMode(speakerPIN, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(speakerPIN, HIGH);
+  delay(500);
+  digitalWrite(speakerPIN, LOW);
+  delay(500);
 }
